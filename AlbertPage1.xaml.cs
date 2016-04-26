d31aa6f33ce2ff830d64f7e149c774ebcefd8414
scripter <t279507736@outlook.com>
2016-04-26 16:13:09 +0800

    Added files via upload

---
+++ AlbertPage1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上提供
+
+namespace Analysis_System
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class AlbertPage1 : Page
+    {
+        public AlbertPage1()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            stackpanel.Visibility = Visibility.Visible;
+        }
+        private void change()
+        {
+            ;
+        }
+
+    }
+}
